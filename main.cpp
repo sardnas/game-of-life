83fdd6f9cd2098ff9bd7e6730fefeb63a0420021
sardnas <sandrareinecke19@gmail.com>
2023-03-13 15:02:39 +0100

    removed truecoordinate doubles

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@
 #include <vector>
 #include <chrono>
 #include <thread>
+#include <algorithm>
 
 using namespace std;
 
 const int columns = 30;
 const int rows = 30;
-vector<pair<int, int>> trueCoordinates;
 
 void delay() {
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
 }
 
-vector<vector<bool>> initialGrid(int numberOfActiveCoordinates){
+vector<vector<bool>> initialGrid(int numberOfActiveCoordinates, vector<pair<int, int>>* trueCoordinates){
     vector<vector<bool>> grid(rows, vector<bool>(columns, false));
     string input = "";
     int x = 0;
@@ -54,7 +54,7 @@ vector<vector<bool>> initialGrid(int numberOfActiveCoordinates){
         }
 
         grid[x-1][y-1] = true;
-        trueCoordinates.push_back(make_pair(x-1, y-1));
+        trueCoordinates->push_back(make_pair(x-1, y-1));
     }
 
     return grid;
@@ -101,7 +101,7 @@ string getOutPutGrid(vector<vector<bool>> grid){
 // rules for game of life:
 // stay a live if 2-3 live neighbours, otherwise dead
 // born if a dead cell has exact 3 neighbours
-vector<vector<bool>> calculateNewGrid(vector<vector<bool>> grid){
+vector<vector<bool>> calculateNewGrid(vector<vector<bool>> grid, vector<pair<int, int>>* trueCoordinates){
     vector<vector<bool>> newGrid(grid.size(), vector<bool>(grid[0].size(), false));
     vector<pair<int, int>> newTrueCoordinates;
     vector<pair<int, int>> deadCellsToCheck;
@@ -109,9 +109,10 @@ vector<vector<bool>> calculateNewGrid(vector<vector<bool>> grid){
     int grid_height = grid.size();
 
     int counter = 0;
+    
 
     // check which of the live coordinates can keep living
-    for (auto& p : trueCoordinates) {
+    for (auto& p : *trueCoordinates) {
         int x = p.first;
         int y = p.second;
 
@@ -120,71 +121,64 @@ vector<vector<bool>> calculateNewGrid(vector<vector<bool>> grid){
         int xp = (x+1 > grid_width - 1? 0 : x+1);
         int yp = (y+1 > grid_height - 1? 0 : y+1);
 
-/*
-        cout << "True cells: " << endl;
-        cout << "xm: " << xm << endl;
-        cout << "ym: " << ym << endl;
-        cout << "xp: " << xp << endl;
-        cout << "yp: " << yp << endl;
-*/
         if(grid[xm][ym]){
             counter++;
-        }else{
+        }else if(!grid[xm][ym] || find(deadCellsToCheck.begin(), deadCellsToCheck.end(), make_pair(xm,ym)) == deadCellsToCheck.end()){
             deadCellsToCheck.push_back(make_pair(xm, ym));
         }
 
         if(grid[xm][y]){
             counter++;
-        }else{
+        }else if(!grid[xm][y] || find(deadCellsToCheck.begin(), deadCellsToCheck.end(), make_pair(xm,y)) == deadCellsToCheck.end()){
             deadCellsToCheck.push_back(make_pair(xm, y));
         }
 
         if(grid[xm][yp]){
             counter++;
-        }else{
+        }else if(!grid[xm][yp] || find(deadCellsToCheck.begin(), deadCellsToCheck.end(), make_pair(xm,yp)) == deadCellsToCheck.end()){
             deadCellsToCheck.push_back(make_pair(xm, yp));
         }
 
         if(grid[x][yp]){
             counter++;
-        }else{
+        }else if(!grid[x][yp] || find(deadCellsToCheck.begin(), deadCellsToCheck.end(), make_pair(x,yp)) == deadCellsToCheck.end()){
             deadCellsToCheck.push_back(make_pair(x, yp));
         }
 
         if(grid[xp][yp]){
             counter++;
-        }else{
+        }else if(!grid[xp][yp] || find(deadCellsToCheck.begin(), deadCellsToCheck.end(), make_pair(xp,yp)) == deadCellsToCheck.end()){
             deadCellsToCheck.push_back(make_pair(xp, yp));
         }
 
         if(grid[xp][y]){
             counter++;
-        }else{
+        }else if(!grid[xp][y] || find(deadCellsToCheck.begin(), deadCellsToCheck.end(), make_pair(xp,y)) == deadCellsToCheck.end()){
             deadCellsToCheck.push_back(make_pair(xp, y));
         }
 
         if(grid[xp][ym]){
             counter++;
-        }else{
+        }else if(!grid[xp][ym] || find(deadCellsToCheck.begin(), deadCellsToCheck.end(), make_pair(xp,ym)) == deadCellsToCheck.end()){
             deadCellsToCheck.push_back(make_pair(xp, ym));
         }
 
         if(grid[x][ym]){
             counter++;
-        }else{
+        }else if (!grid[x][ym] || find(deadCellsToCheck.begin(), deadCellsToCheck.end(), make_pair(x,ym)) == deadCellsToCheck.end()){
             deadCellsToCheck.push_back(make_pair(x, ym));
         }
 
         if(counter > 1 && counter < 4){
             newGrid[x][y] = true;
-            newTrueCoordinates.push_back(make_pair(x, y));
+            if((find(newTrueCoordinates.begin(), newTrueCoordinates.end(), make_pair(x,y)) == newTrueCoordinates.end())){
+                newTrueCoordinates.push_back(make_pair(x, y));
+            }
         }
-        //cout << counter << endl;
+
         counter = 0;
-        //cout << counter << endl;
     }
 
-    // check which coordinates to be born
     for (auto& p : deadCellsToCheck) {
         int x = p.first;
         int y = p.second;
@@ -193,13 +187,7 @@ vector<vector<bool>> calculateNewGrid(vector<vector<bool>> grid){
         int ym = (y-1 < 0 ? grid_height - 1: y-1);
         int xp = (x+1 > grid_width - 1? 0 : x+1);
         int yp = (y+1 > grid_height - 1? 0 : y+1);
-        /*
-        cout << "Dead cells: " << endl;
-        cout << "xm: " << xm << endl;
-        cout << "ym: " << ym << endl;
-        cout << "xp: " << xp << endl;
-        cout << "yp: " << yp << endl;
-*/
+
         counter = 0;
         if(grid[xm][ym]){
             counter++;
@@ -234,21 +222,78 @@ vector<vector<bool>> calculateNewGrid(vector<vector<bool>> grid){
         }
 
         if(counter == 3){
+
             newGrid[x][y] = true;
-            newTrueCoordinates.push_back(make_pair(x, y));
+            if((find(newTrueCoordinates.begin(), newTrueCoordinates.end(), make_pair(x,y)) == newTrueCoordinates.end())){
+                newTrueCoordinates.push_back(make_pair(x, y));
+            }
         }
     }
 
     // reset trueCoordinates to the new set of true coordinates
-    deadCellsToCheck.clear();
-    trueCoordinates = newTrueCoordinates;
+    trueCoordinates->clear();  
+    *trueCoordinates = newTrueCoordinates;
 
     return newGrid;
 }
 
+vector<vector<bool>> defaultGrids(vector<pair<int, int>>* trueCoordinates){
+    vector<vector<bool>> grid(rows, vector<bool>(columns, false));
+    string input = "";
+    int x = 0;
+    int model = 0;
+
+    while (true) {
+        cout << "1. Glider\n2. Pulsar\n3. Toad\n4. Spaceship\nWhich pattern would you like to begin with (1,2,3,4)?: ";
+        cin >> input;
+        try {
+            x = stoi(input);
+            if (x < 1 || x > 4) {
+                throw out_of_range("Invalid input.");
+            }
+            if(x == 1){
+                grid[5][5] = true;
+                grid[6][5] = true;
+                grid[7][5] = true;
+                grid[7][4] = true;
+                grid[6][3] = true;
+                trueCoordinates->push_back(make_pair(5,5));
+                trueCoordinates->push_back(make_pair(6,5));
+                trueCoordinates->push_back(make_pair(7,5));
+                trueCoordinates->push_back(make_pair(7,4));
+                trueCoordinates->push_back(make_pair(6,3));
+            }
+            if(x == 2){ // todo
+                grid[5][5] = true;
+                grid[6][5] = true;
+                grid[7][5] = true;
+            }
+            if(x == 3){ //todo
+                grid[5][5] = true;
+                grid[6][5] = true;
+                grid[7][5] = true;
+            }
+            if(x == 4){ //todo
+                grid[5][5] = true;
+                grid[6][5] = true;
+                grid[7][5] = true;
+            }
+            break;
+        }
+        catch (const std::exception& e) {
+            cout << "Invalid input. Please enter a valid number." << endl;
+        }
+    }
+
+
+    return grid;
+}
+
+
 int main() {
-    int activeCoordinates = initialActiveCoordinates();
-    vector<vector<bool>> grid = initialGrid(activeCoordinates);
+    vector<pair<int, int>> trueCoordinates;
+    //int activeCoordinates = initialActiveCoordinates();
+    vector<vector<bool>> grid = defaultGrids(&trueCoordinates);//initialGrid(activeCoordinates, &trueCoordinates);
     string outputGrid = getOutPutGrid(grid);
     vector<vector<bool>> newGrid;
 
@@ -256,9 +301,9 @@ int main() {
         cout << " " << endl;
         cout << outputGrid;
         delay();
-        newGrid = calculateNewGrid(grid);
+        newGrid = calculateNewGrid(grid, &trueCoordinates);
         if(grid == newGrid){
-            break;
+                break;
         }
         grid = newGrid;
         outputGrid = getOutPutGrid(grid);
